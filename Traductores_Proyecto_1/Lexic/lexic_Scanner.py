a0280daf728a08d7fc008f44cc0ba7f2050cbe6e
Francisco Sucre <frank91frank@gmail.com>
2015-09-22 20:03:31 -0430

    Se Definieron algunas tokes

--- Traductores_Proyecto_1/Lexic/lexic_Scanner.py
+++ Traductores_Proyecto_1/Lexic/lexic_Scanner.py
@@ -6,23 +6,31 @@ Created on 22 de sept. de 2015
 
 import ply.lex as lex
 
-tokens = [ 'NAME','NUMBER','PLUS','MINUS','TIMES',
-           'DIVIDE', 'EQUALS' ]
-
-literals = ['=','+','-','*','/', '(',')']
-
-t_ignore = ' \t'         
-t_PLUS   = r'\+'
-t_MINUS  = r'-'
-t_TIMES  = r'\*'
-t_DIVIDE = r'/'
-t_EQUALS = r'='
-t_NAME   = r'[a-zA-Z_][a-zA-Z0-9_]*'
+tokens = ['TkComa','TkPunto','TkDosPuntos','TkParAbre','TkParCierra','TkSuma','TkResta',
+          'TkMult','TkDiv','TkMod','TkConjuncion','TkDisyuncion','TkNegacion','TkMenor',
+          'TkMenorIgual','TkMayor','TkMayorIgual','TkIgual']
 
 
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
+
+
+t_TkComa = r','
+t_TkPunto = r'\.'
+t_TkDosPuntos = r':'
+t_TkParAbre = r'\(' 
+t_TkParCierra = r'\)'
+t_TkSuma = r'\+' 
+t_TkResta = r'-' 
+t_TkMult = r'\*' 
+t_TkDiv = r'/' 
+t_TkMenor = r'<' 
+t_TkMenorIgual = r'<=' 
+t_TkMayor = r'>' 
+t_TkMayorIgual = r'>=' 
+t_TkIgual = r'=' 
+
+# Caracteres Ignorados
+
+t_ignore = " \t"
+
 
 lex.lex()
