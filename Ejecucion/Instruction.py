addd5406d571d4d7b600e1579f67f13a3240e685
franciscosucre <frank91frank@gmail.com>
2016-03-24 00:44:36 -0430

    (FJSG) Documentacion

--- Ejecucion/Instruction.py
+++ Ejecucion/Instruction.py
@@ -18,28 +18,63 @@ from Ejecucion.Expression import BooleanExpression
 from Ejecucion.Expression import ParentizedExpression
 
 BotSymbolTable = SymbolTable(None);
-posicionMatrix = dict()
+posicionmatriz = dict()
 currentBotHorPosicion = None
 currentBotVerPosicion = None
 currentBotValue = None
 
-def spacing(espacio):
-    i = 0
-    while (i < 4):
-        espacio += " " 
-        i += 1
-    return espacio
-
-def getValueInPosicionMatrix(matrix,horPosicion,verPosicion):
-    stringPosicion = "(" + str(horPosicion) + "," + str(verPosicion) + ")"
-    return matrix.get(stringPosicion)
+def getValueInPosicionmatriz(matriz,horPosicion,verPosicion):
+    """
+    Toma una matriz y unas coordenadas y obtiene el valor almacenado
+    en esas coordenadas
     
-def setValueInPosicionMatrix(matrix,horPosicion,verPosicion,element):
+    @type  matriz: Dict
+    @param matriz: Matriz de recorrido de los bots
+    @type  horPosicion: int
+    @param horPosicion: posicion en el eje horizontal de la matriz
+    @type  verPosicion: int
+    @param verPosicion: posicion en el eje vertical de la matriz
+    @rtype: Object
+    @return: valor almacenado en la posicion deseada de la matriz
+    """
+    # Formamos el string de coordenadas
     stringPosicion = "(" + str(horPosicion) + "," + str(verPosicion) + ")"
-    matrix.update({stringPosicion : element})
-    return matrix
+    # Retornamos el valor
+    return matriz.get(stringPosicion)
+    
+def setValueInPosicionmatriz(matriz,horPosicion,verPosicion,element):
+    """
+    Toma una matriz y unas coordenadas y guarda un valor en la celda
+    apuntada por las coordenadas
+    
+    @type  matriz: Dict
+    @param matriz: Matriz de recorrido de los bots
+    @type  horPosicion: int
+    @param horPosicion: posicion en el eje horizontal de la matriz
+    @type  verPosicion: int
+    @param verPosicion: posicion en el eje vertical de la matriz
+    @type  element: Object
+    @param element: Valor a ser almacenado
+    @rtype: Dict
+    @return: Matriz actualizada
+    """
+    # Formamos el string de coordenadas
+    stringPosicion = "(" + str(horPosicion) + "," + str(verPosicion) + ")"
+    # Guardamos el elemento en la matriz
+    matriz.update({stringPosicion : element})
+    # Retornamos la matriz actualizada
+    return matriz
 
 def obtainValueFromString(stringElement):
+    """
+    Toma un string y lo transforma en el tipo primitivo que corresponda
+    Puede ser bool, str o int
+    
+    @type  stringElement: Str
+    @param stringElement: Elemento a transformar
+    @rtype: Object
+    @return: Objeto primitivo
+    """
     global BotSymbolTable
     charPattern = r"'(.)'|'(\\n)'|'(\\t)'|'\\''"
     numPattern = compile('([0-9]+)|(-[0-9]+)')
@@ -76,11 +111,9 @@ class InstructionClass(metaclass=ABCMeta):
         pass
     
 class Program(InstructionClass):
-    
     def __init__(self, executeSet, createSet=None):
         self.createSet = createSet
         self.executeSet = executeSet
-        
     def __str__(self):
         retorno = ""
         retorno += "\n"
@@ -99,13 +132,12 @@ class Program(InstructionClass):
             retorno += str(execute)
         retorno += "\n"
         return retorno
-    
-    # Corrida de la instruccion
     def run(self):
+        """
+        Corrida de la instruccion
+        """
         global BotSymbolTable
-        global posicionMatrix
-        
-        
+        global posicionmatriz
         # Revisamos si hay declaraciones de bots
         if self.createSet != None:
             # Revisamos si estamos en una tabla vacia
@@ -114,29 +146,26 @@ class Program(InstructionClass):
             # Si no es asi, bajamos a un nivel de tabla inferior
             else:
                 BotSymbolTable = SymbolTable(deepcopy(BotSymbolTable))
-            
+            # Reiniciamos el iterador
             i = 0
-               
+            # Recorremos las declaraciones de los bots
             for i in  range(0,len(self.createSet)):
                 (self.createSet[i]).run()
-        
+        # Reiniciamos el iterador
         i = 0
-          
+        # Recorremos las instrucciones del programa
         for i in  range(0,len(self.executeSet)):
             (self.executeSet[i]).run()
-        
         # Si no estamos en la tabla tope subimos un nivel
         if BotSymbolTable.getUpperLevel() != None:
             BotSymbolTable.getUpperLevel()
         
 # Clase Crear el LexBot 
 class CreateInstruction(InstructionClass):
-
     def __init__(self, botType,identifier,declarationSet ):
         self.botType = botType
         self.identifier = identifier
         self.declarationSet = declarationSet    
-
     def __str__(self):
         espacio = ""
         retorno = ""
@@ -151,24 +180,23 @@ class CreateInstruction(InstructionClass):
                 retorno += espacio + espacio + espacio + espacio +  str(declarion)
         retorno += "\n"
         return retorno
-
-    # Corrida de la instruccion
     def run(self):
+        """
+        Corrida de la instruccion
+        """
         global BotSymbolTable
-        global posicionMatrix
-        
+        global posicionmatriz
+        # Creamos el simbolo
         symbol = Symbol(self.identifier,self.botType,None)
         symbol.behaviorTable = self.declarationSet
-        
+        # Agregamos el simbolo a la tabla
         BotSymbolTable.addToTable(self.identifier, symbol)
 
 # Clase Declaracion del bot
 class BotBehavior(InstructionClass):
-    
     def __init__(self,condition,instructionSet ):
         self.condition = condition
         self.instructionSet = instructionSet
- 
     def __str__(self):
         espacio = ""
         retorno = ""
@@ -184,16 +212,15 @@ class BotBehavior(InstructionClass):
                 retorno += espacio + espacio + espacio + espacio + str(instruction)
                 retorno += "\n"
         return retorno
- 
-    # Corrida de la instruccion
     def run(self):
-        
+        """
+        Corrida de la instruccion
+        """
         global BotSymbolTable
         global currentBotType
         global currentBotValue
         global currentBotHorPosicion
         global currentBotVerPosicion
-        
         # Corremos las instrucciones
         for i in  range(0,len(self.instructionSet)):
             self.instructionSet[i].run()        
@@ -203,13 +230,10 @@ class BotBehavior(InstructionClass):
         BotSymbolTable = BotSymbolTable.getUpperLevel()
         return botResult
     
-# Clase Intrucciones del boot
 class BotInstruction(InstructionClass):
-
     def __init__(self, command,argument=None):
         self.command = command
         self.argument = argument
-   
     def __str__(self):
         espacio = ""
         retorno = ""
@@ -224,9 +248,7 @@ class BotInstruction(InstructionClass):
                 retorno +=espacio + espacio + espacio+ espacio + " - instruccion: soltado"  
             retorno += "\n"
             retorno +=espacio + espacio + " - valor: "  + str(self.argument)
-        
         elif (self.argument == None):
-        
             if (self.command == "send"):
                 retorno +=espacio + espacio+ espacio+ espacio + " - instruccion: enviar"
             elif (self.command == "read"):
@@ -239,41 +261,41 @@ class BotInstruction(InstructionClass):
                 retorno +=espacio + espacio+ espacio+ espacio + " - instruccion: movimiento hacia arriba"
             elif (self.command == "down"):
                 retorno +=espacio + espacio + " - instruccion: movimiento hacia abajo"
-        
         retorno += "\n"
-        
         return retorno 
-
     def run(self):
+        """
+        Corrida de la instruccion
+        """
         global BotSymbolTable
-        global posicionMatrix
-        
+        global posicionmatriz
+        # STORE
         if self.command == 'store':
-            
             # Caso 1: El argumento es una expresion
             if (type(self.argument) is ArithmethicExpression or 
                   type(self.argument) is BooleanExpression or
                   type(self.argument) is RelationalExpresion or
                   type(self.argument) is ParentizedExpression):
                 result = self.argument.evaluar(BotSymbolTable)
+            # Caso 2: El argumento es un string de un tipo primitivo
             else:
                 result = obtainValueFromString(self.argument)
-            
+            # Actualizamos el valor del bot
             BotSymbolTable.updateSymbolValue("me",result)
-                
-
+        # COLLECT
         elif self.command == 'collect':
-            
+            # Buscamos los datos actuales del bot
             symbol = BotSymbolTable.searchForSymbol("me")
-            result = getValueInPosicionMatrix(posicionMatrix,symbol.horPosicion,symbol.verPosicion)
-            
+            # Obtenemos el valor de la posicion actual de la matriz
+            result = getValueInPosicionmatriz(posicionmatriz,symbol.horPosicion,symbol.verPosicion)
+            # Si no recibimos argumento lo guardamos en el bot
             if self.argument is None:
-                
                 BotSymbolTable.updateSymbolValue("me",result)
-
             else:
+                # Buscamos a ver si la variable ya fue declarada anteriormente
                 upperlevel = BotSymbolTable.getUpperLevel()
                 symbol = upperlevel.searchForSymbol(self.argument)
+                # Si no encontramos la variable seguimos
                 if not (symbol):
                     symbol = BotSymbolTable.searchForSymbol(self.argument)
                     if symbol != None:
@@ -287,91 +309,96 @@ class BotInstruction(InstructionClass):
                 else:
                     print("ERROR: Variable Declarada anteriormente en un alcance anterior\n")
                     exit()
-
+        # DROP
         elif self.command == 'drop':
+            # Buscamos los datos actuales del bot
             symbol = BotSymbolTable.searchForSymbol("me")
-            
             # Caso 1: El argumento es una expresion
             if (type(self.argument) is ArithmethicExpression or 
                   type(self.argument) is BooleanExpression or
                   type(self.argument) is RelationalExpresion or
                   type(self.argument) is ParentizedExpression):
                 resultado = self.argument.evaluar(BotSymbolTable)
+            # Caso 2: El argumento es un string de un tipo primitivo
             else:
                 resultado = obtainValueFromString(self.argument)
-            
-            posicionMatrix = setValueInPosicionMatrix(posicionMatrix,symbol.horPosicion,symbol.verPosicion,resultado)
-            
+            # Guardamos el valor en la matriz
+            posicionmatriz = setValueInPosicionmatriz(posicionmatriz,symbol.horPosicion,symbol.verPosicion,resultado)
+        # SEND            
         elif self.command == 'send':
-
+            # Buscamos los datos actuales del bot
             symbol = BotSymbolTable.searchForSymbol("me")
+            # Obtenemos el valor del bot
             value = symbol.getValue()
+            # Si es un string lo imprimimos
             if type(value) is str:
                 print(value,end="")
+            # Si no es un string lo transformamos en string antes de imprimirlo
             else:
                 print(str(symbol.getValue()),end="")
-            
+        # READ            
         elif self.command == 'read':
-            
+            # Buscamos los datos actuales del bot
             symbol = BotSymbolTable.searchForSymbol("me")
-            
+            # Obtenemos la entrada del usuario
             stringElement = input("Value To Read " + "(" + symbol.getType()  + " type): ")
-            
+            # Convertimos esa entrada en un tipo primitivo
             element = obtainValueFromString(stringElement)
-            
+            # Caso 1: Se guardara en el bot
             if self.argument is None:
                 pass
+            # Caso 2: Se guardara en una variable
             else:
                 symbol = BotSymbolTable.searchForSymbol(self.argument)
-                
+            # Actualizamos el valor del bot     
             BotSymbolTable.updateSymbolValue(symbol.getIdentifier(),element)
-            
+        # LEFT            
         elif self.command == 'left':
-            
+            # Buscamos los datos actuales del bot
             symbol = BotSymbolTable.searchForSymbol("me")
-            
+            # Caso 1: Se movera 1 paso
             if self.argument is None:
                 BotSymbolTable.updateSymbolHorPosicion("me",symbol.horPosicion - 1)
+            # Caso 2: Se moveran varios pasos
             else:
                 BotSymbolTable.updateSymbolHorPosicion("me",symbol.horPosicion - self.argument.evaluar(BotSymbolTable))
-            
+        # RIGHT            
         elif self.command == 'right':
-
+            # Buscamos los datos actuales del bot
             symbol = BotSymbolTable.searchForSymbol("me")
-            
+            # Caso 1: Se movera 1 paso
             if self.argument is None:
                 BotSymbolTable.updateSymbolHorPosicion("me",symbol.horPosicion + 1)
+            # Caso 2: Se moveran varios pasos
             else:
                 BotSymbolTable.updateSymbolHorPosicion("me",symbol.horPosicion + self.argument.evaluar(BotSymbolTable))
-            
-
+        # UP
         elif self.command == 'up':
-
+            # Buscamos los datos actuales del bot
             symbol = BotSymbolTable.searchForSymbol("me")
-            
+            # Caso 1: Se movera 1 paso
             if self.argument is None:
                 BotSymbolTable.updateSymbolVerPosicion("me",symbol.verPosicion + 1)
+            # Caso 2: Se moveran varios pasos
             else:
                 BotSymbolTable.updateSymbolVerPosicion("me",symbol.verPosicion + self.argument.evaluar(BotSymbolTable))
-            
-
+        # DOWN
         elif self.command == 'down':
-            
+            # Buscamos los datos actuales del bot
             symbol = BotSymbolTable.searchForSymbol("me")
-            
+            # Caso 1: Se movera 1 paso
             if self.argument is None:
                 BotSymbolTable.updateSymbolVerPosicion("me",symbol.verPosicion - 1)
+            # Caso 2: Se moveran varios pasos
             else:
                 BotSymbolTable.updateSymbolVerPosicion("me",symbol.verPosicion - self.argument.evaluar(BotSymbolTable))
 
 # Class ConditionalInstruction      
 class ConditionalInstruction(InstructionClass):
-
     def __init__(self,ifCondition,ifInstructionSet,elseInstructionSet=None):
         self.ifCondition = ifCondition
         self.ifInstructionSet = ifInstructionSet
         self.elseInstructionSet = elseInstructionSet    
-    
     def __str__(self):
         retorno = ""
         retorno += "\n"
@@ -379,20 +406,26 @@ class ConditionalInstruction(InstructionClass):
         retorno += "\n"
         retorno += "    CONDICIONAL"
         retorno += str(self.ifCondition)
+        # Recorremos las instrucciones del if
         for instruction in self.ifInstructionSet :
             retorno += str(instruction)
-        
+        # Si hay else, recorremos las instrucciones del else
         if self.elseInstructionSet != None:
             for instruction in self.elseInstructionSet :
                 retorno += str(instruction)
         return retorno
-
-    # Corrida de la instruccion
     def run(self):
+        """
+        Corrida de la instruccion
+        """
+        # Caso 1: Se cumple la condicion del if
         if (self.ifCondition.evaluar(BotSymbolTable)):
+            # recorremos y corremos las instrucciones
             for i in  range(0,self.ifInstructionSet.len):
                 self.ifInstructionSet[i].run()
+        # Caso 2: Si hay condicion de else y no se cumplio el if
         elif (self.elseInstructionSet):
+            # recorremos y corremos las instrucciones
             for i in  range(0,self.elseInstructionSet.len):
                 self.elseInstructionSet[i].run()
         
@@ -402,7 +435,6 @@ class whileInstruction(InstructionClass):
     def __init__(self,condition,instructionSet):
         self.condition = condition
         self.instructionSet = instructionSet
-        
     def __str__(self):
         retorno = ""
         retorno += "\n"
@@ -414,10 +446,13 @@ class whileInstruction(InstructionClass):
         for instruction in self.instructionSet :
             retorno += str(instruction)
         return retorno
-
-    # Corrida de la instruccion
     def run(self):
+        """
+        Corrida de la instruccion
+        """
+        # Mientras se cumpla la condicion iteramos
         while (self.condition.evaluar(BotSymbolTable)):
+            # Recorremos y corremos las instrucciones
             for i in  range(0,len(self.instructionSet)):
                 self.instructionSet[i].run()
         
@@ -441,8 +476,10 @@ class ActivateInstruction:
             retorno += "\n"
         return retorno
  
-    # Corrida de la instruccion
     def run(self):
+        """
+        Corrida de la instruccion
+        """
         global BotSymbolTable
         global currentBotType
         global currentBotValue
@@ -491,10 +528,8 @@ class ActivateInstruction:
         
 # Class DeactivateInstruction (REVISAR COMO HACER EL FOR PENDIENTE EN DONDE)
 class DeactivateInstruction:
-    
     def __init__(self,identList):
         self.identList = identList
-        
     def __str__(self):
         espacio = "  "
         retorno = ""
@@ -507,9 +542,10 @@ class DeactivateInstruction:
             retorno += espacio + espacio + " - var: " + str(nameBot)
             retorno += "\n"
         return retorno
-
-    # Corrida de la instruccion
     def run(self):
+        """
+        Corrida de la instruccion
+        """
         global BotSymbolTable
         global currentBotType
         global currentBotValue
@@ -561,10 +597,8 @@ class DeactivateInstruction:
 
 # Class AdvanceInstruction
 class AdvanceInstruction:
-
     def __init__(self,identList):
         self.identList = identList
-    
     def __str__(self):
         espacio = "    "
         retorno = ""
@@ -577,9 +611,10 @@ class AdvanceInstruction:
             retorno += espacio + espacio + " - var: " + str(nameBot)
             retorno += "\n"
         return retorno
-        
-    # Corrida de la instruccion
     def run(self):
+        """
+        Corrida de la instruccion
+        """
         global BotSymbolTable
         global currentBotType
         global currentBotValue
